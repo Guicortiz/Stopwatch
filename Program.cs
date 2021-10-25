33a01802643ed6da9ab53ba03057bb422ebc3996
Guicortiz <guilhermechenciortiz@gmail.com>
2021-10-25 20:01:04 -0300

    Make function start

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Stopwatch
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Clear();
+            Start();
+        }
+
+        static void Start()
+        {
+            int time = 10;
+            int currentTime = 0;
+            while (currentTime <= time)
+            {
+                Console.WriteLine(currentTime);
+                currentTime++;
+            }
         }
     }
 }
